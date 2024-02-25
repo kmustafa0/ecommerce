ae8b6e1f331a26d6376471ca2a6c5aa5fb18fd80
MustafaKole <blackunwer0@gmail.com>
2024-02-25 20:10:05 +0300

    added todo

--- src/components/HeaderComponent/AccountNavigation/index.jsx
+++ src/components/HeaderComponent/AccountNavigation/index.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import styles from './index.module.scss';
 
 const AccountNavigation = ({ href, icon, label, subItems }) => {
+  /* TODO eğer giriş yapılı değilse 2 buton alt alta giriş yap üye ol ve eğer sepet boşsa bişeyler ekle butonu  */
   return (
     <div className={styles.container}>
       <Link href={href} className={styles.link}>
