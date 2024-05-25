5993b4bae6561c3c5188f1137c8b8be783628991
MustafaKole <blackunwer0@gmail.com>
2024-05-25 14:41:29 +0300

    check baseurl

--- src/lib/queries.js
+++ src/lib/queries.js
@@ -1,9 +1,9 @@
-'use client';
 import axios from 'axios';
 
 export const getImages = async () => {
   try {
-    const baseURL = process.env.NEXT_PUBLIC_NEXTAUTH_URL;
+    const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+    console.log('baseURL: ', baseURL);
     const response = await axios.get(`${baseURL}/api/heroImages`, {
       cache: 'no-store',
     });
